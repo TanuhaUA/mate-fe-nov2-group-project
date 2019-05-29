dff2cf21bca4ddc297ddad9c12f37568d38e5d8a
Maksim <maksimpooh@gmail.com>
2019-05-29 12:46:12 +0300

    remove pug plugin and pug files

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,11 @@
 var gulp        = require('gulp'),
     scss        = require('gulp-sass'),
-    pug         = require('gulp-pug'),
     browserSync = require('browser-sync');
 
 gulp.task('scss', async () => {
     return gulp.src('src/scss/style.scss')
         .pipe(scss())
         .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('pug', async () => {
-    return gulp.src('src/pug/index.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('src'))
         .pipe(browserSync.reload({stream: true}))
 });
 
@@ -26,12 +18,13 @@ gulp.task('browser-sync', async () => {
     });
 });
 
+gulp.task('html', async () => {
+    return gulp.src('src/index.html')
+        .pipe(browserSync.reload({stream: true}))
+});
+
 gulp.task('scss-watch', async () => {
     gulp.watch('src/scss/*.scss', gulp.parallel('scss'));
 });
 
-gulp.task('pug-watch', async () => {
-    gulp.watch('src/pug/*.pug', gulp.parallel('pug'));
-});
-
-gulp.task('default', gulp.parallel('scss', 'pug', 'browser-sync', 'scss-watch', 'pug-watch'));
+gulp.task('default', gulp.parallel('scss', 'browser-sync', 'html', 'scss-watch'));
